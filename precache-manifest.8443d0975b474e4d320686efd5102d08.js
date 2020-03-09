a3c58315e25adf84520606fd21d7baa8aa839f9c
Mauricio Rodrigues <grtorres17@gmail.com>
2020-03-09 01:38:55 -0300

    Updates

---
+++ precache-manifest.8443d0975b474e4d320686efd5102d08.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9bd3b5d3b9c67c6dfe82fe66dbcfc6de",
+    "url": "/tibianfinder/index.html"
+  },
+  {
+    "revision": "060c39882e12babc1a8c",
+    "url": "/tibianfinder/static/css/2.1bd2ab2f.chunk.css"
+  },
+  {
+    "revision": "060c39882e12babc1a8c",
+    "url": "/tibianfinder/static/js/2.73b8fff8.chunk.js"
+  },
+  {
+    "revision": "eff3b4d7e4bb0087ce53139cd90bc880",
+    "url": "/tibianfinder/static/js/2.73b8fff8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "10635df37175f803b413",
+    "url": "/tibianfinder/static/js/main.4ced2cd8.chunk.js"
+  },
+  {
+    "revision": "5ff33cf57cab66ced4d9",
+    "url": "/tibianfinder/static/js/runtime-main.11a6e0b4.js"
+  },
+  {
+    "revision": "35f575626f6b473afe54a89a99993f9f",
+    "url": "/tibianfinder/static/media/background.35f57562.png"
+  }
+]);
