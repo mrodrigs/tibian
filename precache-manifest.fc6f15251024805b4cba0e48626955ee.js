239ff63ac688c092956dc56fc2b8597c977fd576
Mauricio Rodrigues <grtorres17@gmail.com>
2020-03-09 02:51:42 -0300

    Updates

---
+++ precache-manifest.fc6f15251024805b4cba0e48626955ee.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9f6e53444b928afe259bec22f9b2a8c",
+    "url": "/tibian/index.html"
+  },
+  {
+    "revision": "6e2058e08a552ef13fb1",
+    "url": "/tibian/static/css/2.1bd2ab2f.chunk.css"
+  },
+  {
+    "revision": "6e2058e08a552ef13fb1",
+    "url": "/tibian/static/js/2.263a7533.chunk.js"
+  },
+  {
+    "revision": "eff3b4d7e4bb0087ce53139cd90bc880",
+    "url": "/tibian/static/js/2.263a7533.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bd63d16ab5fa5a25152a",
+    "url": "/tibian/static/js/main.21c4a8b0.chunk.js"
+  },
+  {
+    "revision": "162dfc8fa5555b530457",
+    "url": "/tibian/static/js/runtime-main.da91e9df.js"
+  },
+  {
+    "revision": "35f575626f6b473afe54a89a99993f9f",
+    "url": "/tibian/static/media/background.35f57562.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/tibianfinder/precache-manifest.53f5d439d54501f87cf7b3a05bc13eea.js"
+  "/tibian/precache-manifest.fc6f15251024805b4cba0e48626955ee.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/tibianfinder/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/tibian/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
